1ee90336dd9a7e0913db3240a2dde98a5b71be8a
Frarlon Rodriguez <fr0609@gmail.com>
2015-06-23 11:19:20 +0000

    test for jobs and refactored

---
+++ jobs-data.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var Promise = require("bluebird");
+var Job = mongoose.model('Job');
+
+
+var jobs = [{
+    title: 'Cook',
+    description: 'You will be making bagels'
+}, {
+    title: 'Professional Dancer',
+    description: 'You will be dancing ballet'
+}, {
+    title: 'Angular Developer',
+    description: 'You will be doing great stuff'
+}];
+
+
+var findJobs = function(query){
+    return Promise.cast(mongoose.model('Job').find(query).exec());
+}
+
+exports.findJobs  = findJobs;
+exports.connectDB = Promise.promisify(mongoose.connect, mongoose);
+
+var createJob = Promise.promisify(Job.create, Job);
+
+exports.seedJobs = function() {
+        return findJobs({}).then(function(collection) {
+            if (collection.length === 0) {
+                return Promise.map(jobs, function(job){
+                    return createJob(job);
+                });
+            }
+        });
+
+}
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require("express");
-
+var jobsData = require("./jobs-data.js");
 var app = express();
 
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/api/jobs', function(req, res) {
+   jobsData.findJobs()
+   .then(function(collection){
+        res.send(collection);    
+    });
+});
+
+
 
 app.get('*', function (req, res) {
     res.render('index');
 });
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+jobsData.connectDB('mongodb://psdev:psdev@ds061711.mongolab.com:61711/jobfinder')
+.then(function(){
+     console.log('connected to mongodb sucessfully'); 
+     jobsData.seedJobs();
+});
+
+
 
 app.listen(process.env.PORT, process.env.IP);
 
